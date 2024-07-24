21c35d00b777717d70e06427f4ca77b9852a1371
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-07-24 07:54:03 -0400

    Initial Commit

---
+++ Saude Academia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Saude Academia
+//
+//  Created by Silvanei  Martins on 24/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Saude Academia/Saude_AcademiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saude_AcademiaApp.swift
+//  Saude Academia
+//
+//  Created by Silvanei  Martins on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Saude_AcademiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
